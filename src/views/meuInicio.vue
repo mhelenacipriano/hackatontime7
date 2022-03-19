eaffba7fb0f2b5f45c74b0ba84827cf16a6567c6
laislucasss <laisluccas@hotmail.com>
2022-03-19 13:50:09 -0300

    alteraçao img

--- src/views/meuInicio.vue
+++ src/views/meuInicio.vue
@@ -5,7 +5,7 @@
       Aqui você pode confiar
     </h2>
     <v-img class="img-inicio text-center mb-4" 
-    src="https://www.camara.leg.br/midias/image/2020/07/img20191024124354009-1-768x428.jpg"/>
+    src="https://www.camara.leg.br/midias/image/2020/07/img20191024124354009-1-768x428.jpg">
     </v-img>
     <p> Neste aplicativo você contará com o serviço de monitoramento e checagem de conteúdos duvidosos 
         que esclarece o que é falso ou verdadeiro em mensagens disseminadas pelo celular e pela internet.</p>
