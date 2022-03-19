5a3a0370a457e39682bedd320c63060ede8b2d17
Maria Helena Cipriano <mariahelenatw@gmail.com>
2022-03-19 13:37:39 -0300

    pagina de denuncias com form

--- src/views/paginaDenuncias.vue
+++ src/views/paginaDenuncias.vue
@@ -1,15 +1,100 @@
 <template>
   <v-container class="home">
     <h2 class="text-h5 text-center mb-3 mt-5">
-      Canais de denúncia
+      Recebeu alguma notícia falsa? Denuncie!
     </h2>
+
+    <template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Nome"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+        <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="Link da Fake News"
+      required
+    ></v-text-field>
+
+    
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Denunciar
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    
+  </v-form>
+</template>
+
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'paginaDenuncias'
-}
+  name: 'paginaDenuncias',
+
+
+  data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ]
+      
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      
+  
+  }}
 </script>
 
 <style scoped>
